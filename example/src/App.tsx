f99ba7718fba03d10e906374f73c02c6d7d9dd5e
Sebastian <sebastian@squidex.io>
2021-01-16 19:44:06 +0100

    Initial

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'react-push-preview'
+import { PushPreview, PushPreviewTarget, PushPreviewProps } from 'react-push-preview'
 import 'react-push-preview/dist/index.css'
 
+const ALL_TARGETS: { target: PushPreviewTarget, label: string }[] = [{
+    target: 'DeskopFirefox',
+    label: 'Firefox'
+}, {
+    target: 'DesktopChrome',
+    label: 'Chrome'
+}, {
+    target: 'DesktopMacOS',
+    label: 'MacOS'
+}, {
+    target: 'MobileAndroid',
+    label: 'Android'
+}, {
+    target: 'MobileIOS',
+    label: 'iOS'
+}];
+
+const DEFAULT_PROPS: PushPreviewProps = {
+    message: 'Your Notification Message',
+    iconUrl: 'https://raw.githubusercontent.com/notifo-io/notifo/main/media/logo-square.png',
+    imageUrl: 'https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?fit=crop&w=600',
+    target: 'DesktopChrome',
+    title: 'Your Notification',
+}
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+    const [previewProps, setPreviewProps] = useState<PushPreviewProps>(DEFAULT_PROPS);
+
+    return (
+        <div className='container pt-4'>
+            <h1>Push Preview</h1>
+
+            <div className='row'>
+                <div className='col-5 pt-4'>
+                    <div className='form-group'>
+                        <label htmlFor='appName'>App Name</label>
+
+                        <input className='form-control' id='appName' value={previewProps.appName || ''}
+                            onChange={ev => setPreviewProps(v => ({ ...v, appName: ev.target.value }))} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='subTitle'>Sub Title (iOS only)</label>
+
+                        <input className='form-control' id='subTitle' value={previewProps.subTitle || ''}
+                            onChange={ev => setPreviewProps(v => ({ ...v, subTitle: ev.target.value }))} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='title'>Title</label>
+
+                        <input className='form-control' id='title' value={previewProps.title || ''}
+                            onChange={ev => setPreviewProps(v => ({ ...v, title: ev.target.value }))} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='message'>Message</label>
+
+                        <textarea className='form-control' id='message' value={previewProps.message || ''}
+                            onChange={ev => setPreviewProps(v => ({ ...v, message: ev.target.value }))} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='iconUrl'>Icon URL</label>
+
+                        <input className='form-control' id='iconUrl' value={previewProps.iconUrl || ''}
+                            onChange={ev => setPreviewProps(v => ({ ...v, iconUrl: ev.target.value }))} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='imageUrl'>Image URL</label>
+
+                        <input className='form-control' id='imageUrl' value={previewProps.imageUrl || ''}
+                            onChange={ev => setPreviewProps(v => ({ ...v, imageUrl: ev.target.value }))} />
+                    </div>
+                </div>
+                <div className='col-7'>
+                    <div className='mb-4 text-center'>
+                        <div className='btn-group'>
+                            {ALL_TARGETS.map(x => 
+                                <button key={x.target} type='button' className={targetButtonClass(x.target, previewProps.target)}
+                                    onChange={() => setPreviewProps(v => ({ ...v, target: x.target }))}>
+                                    {x.label}
+                                </button>
+                            )}
+                        </div>
+                    </div>
+
+                    <PushPreview {...previewProps} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function targetButtonClass(target: PushPreviewTarget, currentTarget?: PushPreviewTarget) {
+    let clazz = 'btn btn-outline-primary';
+
+    if (target === currentTarget) {
+        clazz += ' active';
+    }
+
+    return clazz;
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,127 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+export type PushPreviewTarget = 'DesktopChrome' | 'DeskopFirefox' | 'DesktopMacOS' | 'MobileAndroid' | 'MobileIOS';
+
+export interface PushPreviewProps {
+    // The name of the app (for mobile push).
+    appName?: string;
+
+    // The target platform.
+    target?: PushPreviewTarget;
+
+    // The title to be displayed.
+    title: string;
+
+    // The optional message.
+    message?: string;
+
+    // The optional sub title (iOS only).
+    subTitle?: string;
+
+    // The url to the icon.
+    iconUrl?: string;
+
+    // The url to the image.
+    imageUrl?: string;
+
+    buttons?: readonly PushPreviewButton[];
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+interface PushPreviewButton {
+    // The label displayed to the user for the action button.
+    title: string;
+
+    // The URL of a small icon for the action button. An alternative to icons is to include emoticons or other symbols inside the title.
+    iconUrl?: string;
 }
+
+export const PushPreview = (props: PushPreviewProps) => {
+    const {
+        appName = '',
+        title,
+        message,
+        iconUrl,
+        buttons,
+        imageUrl,
+        subTitle,
+    } = props;
+
+    return (
+        <div className={styles.test}>
+            <Image className={styles.image} src={imageUrl} />
+
+
+            <div>
+                <Image className={styles.icon} src={iconUrl} fallbackSrc='https://raw.githubusercontent.com/notifo-io/notifo/main/media/logo-square.png' />
+                
+                <span>{appName || 'Your App'}</span>
+            </div>
+
+            {title &&
+                <div>{title}</div>
+            }
+
+            {subTitle &&
+                <div>{subTitle}</div>
+            }
+
+            {message &&
+                <div>{message}</div>
+            }
+
+            {buttons &&
+                <div>
+                    {buttons.map((button, i) => 
+                        <div key={i}>
+                            <Image src={button.iconUrl} />
+
+                            {button.title}
+                        </div>
+                    )}
+                </div>
+            }
+        </div>
+    );
+}
+
+interface ImageProps {
+    className?: string;
+
+    // The fallback source.
+    fallbackSrc?: string;
+
+    // The size of the icon.
+    src?: string;
+}
+
+const Image = ({ src, fallbackSrc: fallback, className }: ImageProps) => {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const [imageSource, setImageSource] = React.useState(src);
+
+    React.useEffect(() => {
+        setImageSource(src || fallback);
+        setIsLoaded(false);
+    }, [src, fallback]);
+
+    const setLoaded = React.useCallback(() => {
+        if (src === imageSource || !imageSource) {
+            setIsLoaded
+        }
+        setIsLoaded(true);
+    }, [imageSource, src]);
+
+    const onError = React.useCallback(() => {
+        if (fallback) {
+            setImageSource(fallback);
+        }
+    }, [fallback]);
+
+    const style = isLoaded ? {} : { display: 'none' };
+
+    return (
+        <div className={className} style={style}>
+            <img src={src} onLoad={setLoaded} onError={onError} />
+        </div>
+    );
+};
