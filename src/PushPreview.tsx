bc17f8cb889773006e7e990e8789174ac1bbe8b6
Sebastian <sebastian@squidex.io>
2021-01-26 20:24:01 +0100

    Do not use icon fallback for Notifo.

--- src/PushPreview.tsx
+++ src/PushPreview.tsx
@@ -26,14 +26,14 @@ export class PushPreview extends React.Component<PushPreviewProps, State> {
   public render() {
     const { iconUrl, target, ...other } = this.props;
 
-    const url = iconUrl || DEFAULT_LOGO;
+    const iconUrlOrDefault = iconUrl || DEFAULT_LOGO;
 
     return (
       <div className="preview-container">
         {target === 'MobileIOS' && (
           <IOSPreview
             {...other}
-            iconUrl={url}
+            iconUrl={iconUrlOrDefault}
             expanded={this.state.isExpanded}
             toggle={this.toggle}
           />
@@ -42,13 +42,13 @@ export class PushPreview extends React.Component<PushPreviewProps, State> {
         {target === 'MobileAndroid' && (
           <AndroidPreview
             {...other}
-            iconUrl={url}
+            iconUrl={iconUrlOrDefault}
             expanded={this.state.isExpanded}
             toggle={this.toggle}
           />
         )}
 
-        {target === 'Notifo' && <NotifoPreview {...other} iconUrl={url} />}
+        {target === 'Notifo' && <NotifoPreview {...other} iconUrl={iconUrl} />}
       </div>
     );
   }
